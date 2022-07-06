74c82782dbd9ceae7f0861ebcd6118fc6ea0455e
jawsh-me <76971056+jawsh-me@users.noreply.github.com>
2022-07-06 11:13:18 -0400

    Add initial files
    Initial files

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+	internal class Program {
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Type NAME:");
+			var name = Console.ReadLine();
+			if ((name == "NAME") || (name == "NAME:")) {
+				Console.WriteLine("You're way to literal. Quit being a numpty and try again.");
+				Console.WriteLine("Type your name. And don't you dare type \"your name\" you numpty.");
+				name = Console.ReadLine();
+				if (name == "your name") {Console.WriteLine("..... Fine. You win.");}
+			}
+			Console.WriteLine("Hello, {0}.", name);
+			Console.WriteLine("Press the ANY key to continue...");
+			Console.ReadKey();
+		}	
+	}
+}
